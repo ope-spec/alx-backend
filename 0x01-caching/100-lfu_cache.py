133408402a03f7cf419b631a0c2de9db0560ab6b
Opeyemi Ogunniyi <opeyemiogunniyi230@gmail.com>
2024-01-03 16:46:07 +0100

    Code updated

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,35 +10,53 @@ class LFUCache(BaseCaching):
     def __init__(self):
         """ Initialize the LFU Cache"""
         super().__init__()
+        self.usage = []
         self.frequency = {}
-        self.order = []
 
     def put(self, key, item):
         """ Add an item in the cache"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                min_freq = min(self.frequency.values())
-                candidates = [k for k, v in self.frequency.items()
-                              if v == min_freq]
-                if len(candidates) > 1:
-                    lru_key = self.order.pop(0)
-                    candidates.remove(lru_key)
-                    print("DISCARD:", lru_key)
-                discarded_key = candidates[0]
-                del self.cache_data[discarded_key]
-                del self.frequency[discarded_key]
-                print("DISCARD:", discarded_key)
 
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu = min(self.frequency.values())
+                lfu_keys = []
+                for k, v in self.frequency.items():
+                    if v == lfu:
+                        lfu_keys.append(k)
+
+                if len(lfu_keys) > 1:
+                    lru_lfu = {}
+                    for k in lfu_keys:
+                        lru_lfu[k] = self.usage.index(k)
+                    discard = min(lru_lfu.values())
+                    discard = self.usage[discard]
+                else:
+                    discard = lfu_keys[0]
+
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.usage[self.usage.index(discard)]
+                del self.frequency[discard]
+
+            if key in self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
+
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
             self.cache_data[key] = item
-            self.order.append(key)
-            self.frequency[key] = self.frequency.get(key, 0) + 1
 
     def get(self, key):
         """ Get an item by key"""
-        if key is not None:
-            if key in self.cache_data:
-                self.order.remove(key)
-                self.order.append(key)
-                self.frequency[key] = self.frequency.get(key, 0) + 1
-                return self.cache_data[key]
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.frequency[key] += 1
+            return self.cache_data[key]
         return None
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -10,17 +10,25 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """ Initialize the LIFO Cache"""
         super().__init__()
+        self.order = []
 
     def put(self, key, item):
         """ Add an item in the cache"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                discarded_key = next(reversed(self.cache_data))
-                del self.cache_data[discarded_key]
-                print("DISCARD:", discarded_key)
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[-1]))
+                del self.cache_data[self.order[-1]]
+                del self.order[-1]
+            if key in self.order:
+                del self.order[self.order.index(key)]
+            self.order.append(key)
             self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key"""
-        if key is not None:
-            return self.cache_data.get(key)
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
